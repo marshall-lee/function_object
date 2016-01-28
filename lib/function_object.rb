959cdfe815cea7e34de8fca7d3b2b130349f168b
Vladimir Kochnev <hashtable@yandex.ru>
2016-01-28 06:33:49 +0300

    Extract statement compilation to Macro class.

--- lib/function_object.rb
+++ lib/function_object.rb
@@ -1,65 +1,14 @@
 class FunctionObject
   require 'function_object/version'
   require 'function_object/arguments_builder'
+  require 'function_object/macro'
 
   class << self
     def arguments(&block)
       arg_descs = ArgumentsBuilder.new.build(&block)
-      has_defaults = arg_descs.any?(&:default?)
-
-      without_defaults = arg_descs.take_while { |d| !d.default? }
-      with_defaults = arg_descs[without_defaults.length .. -1]
-
-      mixin = Module.new
-
-      mixin.module_eval do
-        with_defaults.each do |desc|
-          private define_method("_default_#{desc.name}", &desc.default)
-        end
-      end if has_defaults
-
-      mixin.module_eval do
-        arg_descs.each do |desc|
-          attr_reader desc.name
-        end
-      end
-
-      arg_list_stmts = without_defaults.map(&:name)
-      arg_list_stmts << '*_args' if has_defaults
-      arg_list_stmt = arg_list_stmts.join(',')
-
-      assign_stmts = []
-      without_defaults.each do |desc|
-        assign_stmts << "@#{desc.name} = #{desc.name}"
-      end
-      with_defaults.each_with_index do |desc, i|
-        assign_stmts << <<-RUBY
-          @#{desc.name} = if #{i} < _args.length
-                            _args[#{i}]
-                          else
-                            _default_#{desc.name}
-                          end
-        RUBY
-      end
-      assign_stmt = assign_stmts.join($/)
-
-      mixin.module_eval <<-RUBY, __FILE__, __LINE__ + 1
-        def initialize(#{arg_list_stmt})
-          #{assign_stmt}
-        end
-      RUBY
-
-      include mixin
-
-      smixin = Module.new
-
-      smixin.module_eval <<-RUBY, __FILE__, __LINE__ + 1
-        def call(#{arg_list_stmt})
-          new(#{arg_list_stmt}).call
-        end
-      RUBY
-
-      singleton_class.class_eval { include smixin }
+      macro = Macro.new(arg_descs)
+      self.class_eval { include macro.class_mixin }
+      singleton_class.class_eval { include macro.sclass_mixin }
     end
     alias_method :args, :arguments
   end
---
+++ lib/function_object/macro.rb
@@ -0,0 +1,74 @@
+class FunctionObject::Macro
+  def initialize(arg_descs)
+    @arg_descs = arg_descs
+    @has_defaults = arg_descs.any?(&:default?)
+
+    @arg_descs_without_defaults = arg_descs.take_while { |d| !d.default? }
+    @arg_descs_with_defaults = arg_descs[arg_descs_without_defaults.length .. -1]
+  end
+
+  def class_mixin
+    Module.new.tap do |mixin|
+      arg_descs_with_defaults.each do |desc|
+        mixin.module_eval do
+          private define_method("_default_#{desc.name}", &desc.default)
+        end
+      end
+
+      arg_descs.each do |desc|
+        mixin.module_eval do
+          attr_reader desc.name
+        end
+      end
+
+      mixin.module_eval <<-RUBY, __FILE__, __LINE__ + 1
+        def initialize(#{arg_list_stmt})
+          #{ivar_assign_stmt}
+        end
+      RUBY
+    end
+  end
+
+  def sclass_mixin
+    Module.new.tap do |mixin|
+      mixin.module_eval <<-RUBY, __FILE__, __LINE__ + 1
+        def call(#{arg_list_stmt})
+          new(#{arg_list_stmt}).call
+        end
+      RUBY
+    end
+  end
+
+  private
+
+  def arg_list_stmt
+    stmts = arg_descs_without_defaults.map(&:name)
+    stmts << '*_args' if defaults?
+    stmts.join(',')
+  end
+
+  def ivar_assign_stmt
+    stmts = []
+    arg_descs_without_defaults.each do |desc|
+      stmts << "@#{desc.name} = #{desc.name}"
+    end
+    arg_descs_with_defaults.each_with_index do |desc, i|
+      stmts << <<-RUBY
+        @#{desc.name} = if #{i} < _args.length
+                          _args[#{i}]
+                        else
+                          _default_#{desc.name}
+                        end
+      RUBY
+    end
+    stmts.join($/)
+  end
+
+  attr_reader :arg_descs,
+              :arg_descs_with_defaults,
+              :arg_descs_without_defaults
+
+  def defaults?
+    @has_defaults
+  end
+end
